c1aaf3acd5538b147d8511ca1e74b6458dafffd8
LiuTujun <liutujun2001@gmail.com>
2024-04-29 16:36:07 +0800

    初始化超级点评后端工程

---
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hmdp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HmDianPingApplicationTests {
+
+    @Test
+    public void testString(){
+        StringBuilder sb = new StringBuilder();
+        //sb.deleteCharAt()
+    }
+
+}
